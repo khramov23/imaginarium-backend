29e9834fd0074ef261647cc0618a3524353eefb6
khramov23 <79625109971@ya.ru>
2023-01-02 17:50:17 +0300

    add mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {MongooseModule} from "@nestjs/mongoose";
+import {getMongoConfig} from "./config/mongo.config";
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: getMongoConfig
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
