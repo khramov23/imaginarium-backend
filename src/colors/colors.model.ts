5f7698349999672b4d1e509ddf6d8c5146ce8249
khramov23 <79625109971@ya.ru>
2023-01-06 00:52:00 +0300

    fix colors model

--- src/colors/colors.model.ts
+++ src/colors/colors.model.ts
@@ -1,66 +1,31 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import {HydratedDocument, Types} from 'mongoose';
-import {Role} from "../role/role.model";
-import {User} from "../user/user.model";
-import {Image} from "../image/models/image.model";
-import {IsInt, Length, Max, Min} from "class-validator";
+import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
+import {HydratedDocument} from 'mongoose';
 
 export type ColorsDocument = HydratedDocument<Colors>;
 
 @Schema()
 export class Colors {
     @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
     red: number;
 
-    @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
     @Prop()
     green: number;
 
     @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
-    @Prop()
     blue: number;
 
-    @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
     @Prop()
     cyan: number;
 
     @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 255'})
-    @Max(255, {message: 'Max value is 0'})
-    @Prop()
     pink: number;
 
-    @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
     @Prop()
     yellow: number;
 
     @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
-    @Prop()
     black: number;
 
-    @Prop()
-    @IsInt({message: "Color should be an integer"})
-    @Min(0, {message: 'Min value is 0'})
-    @Max(255, {message: 'Max value is 255'})
     @Prop()
     white: number;
 }
