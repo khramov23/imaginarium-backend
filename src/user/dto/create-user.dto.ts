090cecbc36e0e1147af7706d439d88ff7e2d251d
khramov23 <79625109971@ya.ru>
2023-01-22 23:43:38 +0300

    change create-user.dto validators

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -6,11 +6,11 @@ export class CreateUserDto {
     readonly email: string
 
     @IsString({message: "Username should be string"})
-    @Length(4, 20, {message: "Username length should be between 4 and 20"})
+    @Length(3, 20, {message: "Username length should be between 3 and 20"})
     readonly username: string
 
     @IsString({message: "Password should be string"})
-    @Length(4, 16, {message: "Password length should be between 4 and 16"})
+    @Length(6, 16, {message: "Password length should be between 6 and 16"})
     readonly password: string
 
 }
