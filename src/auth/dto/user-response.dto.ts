a56e59b9e4db93d4310449253318d23ffd1f6df7
khramov23 <79625109971@ya.ru>
2023-03-10 02:36:18 +0300

    add role to UserResponse

--- src/auth/dto/user-response.dto.ts
+++ src/auth/dto/user-response.dto.ts
@@ -1,28 +1,28 @@
-import {UserDocument} from "../../user/user.model";
-import {Types} from "mongoose";
+import { UserDocument } from '../../user/user.model';
+import { Types } from 'mongoose';
 
 export class UserResponse {
+  _id: Types.ObjectId;
+  username: string;
+  email: string;
+  isActivated: boolean;
+  role: string;
+  followers: Types.ObjectId[];
+  subscriptions: Types.ObjectId[];
+  favorites: Types.ObjectId[];
+  own: Types.ObjectId[];
+  avatar: string;
 
-    _id: Types.ObjectId
-    username: string
-    email: string
-    isActivated: boolean
-    followers: Types.ObjectId[]
-    subscriptions: Types.ObjectId[]
-    favorites: Types.ObjectId[]
-    own: Types.ObjectId[]
-    avatar: string
-
-    constructor(user: UserDocument) {
-        this._id = user._id
-        this.username = user.username
-        this.email = user.email
-        this.isActivated = user.isActivated
-        this.followers = user.followers
-        this.subscriptions = user.subscriptions
-        this.favorites = user.favorites
-        this.own = user.own
-        this.avatar = user.avatar
-    }
-
+  constructor(user: UserDocument) {
+    this._id = user._id;
+    this.username = user.username;
+    this.email = user.email;
+    this.isActivated = user.isActivated;
+    this.role = user.role.value;
+    this.followers = user.followers;
+    this.subscriptions = user.subscriptions;
+    this.favorites = user.favorites;
+    this.own = user.own;
+    this.avatar = user.avatar;
+  }
 }
